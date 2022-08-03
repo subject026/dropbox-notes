9bc78a0fe1ed8a37efda2405fd103a305b73f35e
subject026 <subject026@protonmail.com>
2022-08-03 09:52:01 +0100

    installed type plugin

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,14 @@ import Header from "./components/Header";
 
 const md = new MarkdownIt();
 
+const SOME_MARKDOWN = `
+# Nice Document
+
+hey this is some markdown!
+`;
+
 const App: Component = () => {
-  const [markdown, setMarkdown] = createSignal("");
+  const [markdown, setMarkdown] = createSignal(SOME_MARKDOWN);
 
   const handleKeyup = (event: Event) => {
     const { value } = event.currentTarget as HTMLTextAreaElement;
@@ -14,19 +20,20 @@ const App: Component = () => {
   };
 
   return (
-    <div>
+    <div class="h-screen flex flex-col">
       <Header />
-      <main class="p-4 grid grid-cols-2 gap-4">
-        <textarea
-          class="p-4 rounded"
-          name="markdown"
-          id="markdown"
-          cols="30"
-          rows="10"
-          onKeyUp={handleKeyup}
-        ></textarea>
-        <section innerHTML={md.render(markdown())} class="p-4 rounded" />
+      <main class="grow p-4 grid grid-cols-2 gap-4">
+        <section class="h-full">
+          <textarea
+            class="w-full h-full p-4 rounded"
+            name="editor"
+            id="editor"
+            onKeyUp={handleKeyup}
+          ></textarea>
+        </section>
+        <section innerHTML={md.render(markdown())} class="p-4 rounded prose" />
       </main>
+      <footer class="p-4 bg-purple-200">footer</footer>
     </div>
   );
 };
